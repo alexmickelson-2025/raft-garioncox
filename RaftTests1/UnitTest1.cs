0738eba573f9f312a457c9f45e21abe9ba10c5ae
Garion Cox <garion.cox@students.snow.edu>
2025-01-14 10:01:08 -0700

    Test 11

--- RaftTests1/UnitTest1.cs
+++ RaftTests1/UnitTest1.cs
@@ -1,10 +1,30 @@
+using raft_garioncox;
+
 namespace RaftTests1;
 
-public class UnitTest1
+public class RaftTests1
 {
     [Fact]
-    public void Test1()
+    // Test Case 3
+    public void GivenSingleNode_WhenInitialized_ShouldBeInFollowerState()
     {
-        Assert.True(true);
+        Node n = new(0);
+        Assert.Equal(NODE_STATE.FOLLOWER, n.State);
+    }
+
+    [Fact]
+    // Test Case 11
+    public void GivenSingleNode_WhenItBecomesCandidate_ShouldVoteForItself()
+    {
+        Node n = new(0);
+
+        Assert.Equal(0, n.Term);
+        Assert.False(n.HasVoted);
+
+        n.SetCandidate();
+
+        Assert.Equal(1, n.Term);
+        Assert.Equal(n.Id, n.Vote);
+        Assert.True(n.HasVoted);
     }
 }
--- raft-garioncox/Node.cs
+++ raft-garioncox/Node.cs
@@ -1,6 +1,20 @@
-﻿namespace raft_garioncox;
+﻿using System.Data;
 
-public class Node
+namespace raft_garioncox;
+
+public class Node(int id)
 {
+    public NODE_STATE State { get; private set; } = NODE_STATE.FOLLOWER;
+    public bool HasVoted { get; private set; } = false;
+    public int Id { get; } = id;
+    public int Term { get; private set; } = 0;
+    public int? Vote { get; private set; } = null;
 
+    public void SetCandidate()
+    {
+        State = NODE_STATE.CANDIDATE;
+        Term += 1;
+        Vote = Id;
+        HasVoted = true;
+    }
 }
