61208e3f2685a9a031f06a80f371886d5f2e2970
Garion Cox <garion.cox@students.snow.edu>
2025-01-14 10:29:44 -0700

    Test 8

--- RaftTests1/UnitTest1.cs
+++ RaftTests1/UnitTest1.cs
@@ -1,3 +1,4 @@
+using NSubstitute;
 using raft_garioncox;
 
 namespace RaftTests1;
@@ -6,7 +7,7 @@ public class RaftTests1
 {
     [Fact]
     // Test Case 3
-    public void GivenSingleNode_WhenInitialized_ShouldBeInFollowerState()
+    public void SingleNode_WhenInitialized_ShouldBeInFollowerState()
     {
         Node n = new(0);
         Assert.Equal(NODE_STATE.FOLLOWER, n.State);
@@ -14,7 +15,7 @@ public class RaftTests1
 
     [Fact]
     // Test Case 11
-    public void GivenSingleNode_WhenItBecomesCandidate_ShouldVoteForItself()
+    public void SingleNode_WhenItBecomesCandidate_ShouldVoteForItself()
     {
         Node n = new(0);
 
@@ -26,5 +27,30 @@ public class RaftTests1
         Assert.Equal(1, n.Term);
         Assert.Equal(n.Id, n.Vote);
         Assert.True(n.HasVoted);
+    }
+
+    [Fact]
+    // Test Case 8
+    public void SingleNode_WhenItBecomesCandidate_ShouldBecomeLeader()
+    {
+        Node n = new(0);
+        n.SetCandidate();
+        Assert.Equal(NODE_STATE.LEADER, n.State);
+    }
+
+    [Fact]
+    // Test Case 8
+    public void Cluster_WhenOneBecomesCandidate_ShouldBecomeLeader()
+    {
+        Node n1 = new(0);
+        var n2 = Substitute.For<INode>();
+        var n3 = Substitute.For<INode>();
+        n1.Neighbors = [n2, n3];
+
+        n1.SetCandidate();
+
+        n2.Received().RequestVote(Arg.Any<INode>());
+        n3.Received().RequestVote(Arg.Any<INode>());
+        Assert.Equal(NODE_STATE.LEADER, n1.State);
     }
 }
--- raft-garioncox/Node.cs
+++ raft-garioncox/Node.cs
@@ -2,13 +2,14 @@
 
 namespace raft_garioncox;
 
-public class Node(int id)
+public class Node(int id) : INode
 {
     public NODE_STATE State { get; private set; } = NODE_STATE.FOLLOWER;
     public bool HasVoted { get; private set; } = false;
     public int Id { get; } = id;
     public int Term { get; private set; } = 0;
     public int? Vote { get; private set; } = null;
+    public INode[] Neighbors { get; set; } = [];
 
     public void SetCandidate()
     {
@@ -16,5 +17,17 @@ public class Node(int id)
         Term += 1;
         Vote = Id;
         HasVoted = true;
+
+        foreach (INode node in Neighbors)
+        {
+            node.RequestVote(this);
+        }
+
+        State = NODE_STATE.LEADER;
+    }
+
+    public void RequestVote(INode n)
+    {
+        throw new NotImplementedException();
     }
 }
