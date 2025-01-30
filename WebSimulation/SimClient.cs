d8ff305b5181a8998f5f69e7f595c5a632f47b68
Garion Cox <garion.cox@students.snow.edu>
2025-01-30 16:56:09 -0700

    Client receiving commands in sim

--- WebSimulation/SimClient.cs
+++ WebSimulation/SimClient.cs
@@ -4,10 +4,20 @@ public class SimClient(int id) : IClient
 {
     public int Id { get; private set; } = id;
     public Dictionary<int, INode> Neighbors { get; set; } = [];
+    public Dictionary<string, bool?> CommittedRequests = [];
 
     public Task ReceiveLeaderCommitResponse(string command, bool status)
     {
+        if (status)
+        {
+            CommittedRequests[command] = true;
+        }
+
         return Task.CompletedTask;
-        // throw new NotImplementedException();
+    }
+
+    public void AddCommand(string command)
+    {
+        CommittedRequests[command] = false;
     }
 }
